e592ae734ec8fbe37b6f82cceea7bd87e8487f5d
Tab1999 <103740095+Tab1999@users.noreply.github.com>
2025-08-19 16:36:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,21 @@ function convertToRoman(num) {
     };
 
   //your code here
+	let result = "";
+
+  for (let [roman, value] of symbols) {
+    while (num >= value) {
+      result += roman;
+      num -= value;
+    }
+  }
+
+  return result.toUpperCase();
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
